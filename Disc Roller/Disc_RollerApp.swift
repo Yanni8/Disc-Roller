af47a6c7f640970fb1e5cdbf77b7e49a0db28d26
Yannick Müller <yanni@MacBook-Pro-von-Yannick.local>
2023-01-14 20:34:09 +0100

    add basic app

---
+++ Disc Roller/Disc_RollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disc_RollerApp.swift
+//  Disc Roller
+//
+//  Created by Yannick Müller on 14.01.23.
+//
+
+import SwiftUI
+
+@main
+struct Disc_RollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
